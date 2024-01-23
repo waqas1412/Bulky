74499b9ed5259d673ef75d657d470a0c28a5a871
Waqas <muhammad.waqas1412@gmail.com>
2024-01-23 13:04:21 +0500

    category index page

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -4,6 +4,12 @@ namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
