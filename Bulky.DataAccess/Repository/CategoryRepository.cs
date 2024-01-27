789ec7c9a9277b4d20b74ec2e12fcd943336b036
Waqas <muhammad.waqas1412@gmail.com>
2024-01-27 20:28:45 +0500

    Implemented ICategory Repository Interface

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Save()
+        {
+            dbContext.SaveChanges(); 
+        }
+
+        public void Update(Category category)
+        {
+            dbContext.Categories.Update(category);
+        }
+    }
+}
