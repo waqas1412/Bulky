29bb52da807daef2bb0b6091c3058c53ff77fda4
Waqas <muhammad.waqas1412@gmail.com>
2024-01-27 20:14:09 +0500

    Implemented IRepository Interface

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+        internal DbSet<T> Set;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.Set = dbContext.Set<T>();
+        }
+        public void Add(T item)
+        {
+            Set.Add(item);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = Set;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = Set;
+            return query.ToList();
+        }
+
+        public void Remove(T item)
+        {
+            Set.Remove(item);
+        }
+
+        public void RemoveRange(IEnumerable<T> items)
+        {
+            Set.RemoveRange(items);
+        }
+    }
+}
